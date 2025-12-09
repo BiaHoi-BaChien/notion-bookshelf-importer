9ad6f6fa445c2d606efd601481122c0947b19a0a
Pacho <sugi@clb-biahoi.net>
2025-12-09 14:14:20 +0700

    Revert "Merge pull request #54 from BiaHoi-BaChien/codex/fix-password-key-authentication-after-logout"
    This reverts commit 8e40ed073c6a93c1d5004e53ce6c5562c75e7581, reversing
    changes made to f7af3394ed4a60587c3ec48b63f506beda8986f2.

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
-use App\Http\Controllers\LogoutController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return response()->json(['status' => 'ok']);
 });
-
-Route::post('/logout', LogoutController::class)->name('logout');
